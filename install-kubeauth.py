b10b998a9be83ff7bcfda234889e0820f8366871
Sumit Kumar Maji <skmaji1@outlook.com>
2024-06-19 23:34:37 -0400

    Moved secrets to values

--- install-kubeauth.py
+++ install-kubeauth.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-
-import sys
 import os
 
 def main():
-  sys.stderr.write("Client Id: ")
-  clientId = input()
-
-  sys.stderr.write("Client Secret: ")
-  clientSecret = input()
-
-  sys.stderr.write("Docker User: ")
-  dockerUser = input()
-
-  sys.stderr.write("Docker Password: ")
-  dockerPwd = input()
-
   os.chmod('./run_kubeauth.sh', 0o700)
-
-  os.system("./run_kubeauth.sh -i "+clientId+" -s "+clientSecret+" -u "+dockerUser+" -p "+dockerPwd+" -a "+"keycloak"+" -r "+"GokDevelopers")
-
+  os.system("./run_kubeauth.sh")
 
 if __name__ == '__main__':
   main()
